906c476b6a454f4bb4f3ba3e167697618121c71b
Edinson_Salamanca <sennin7k@gmail.com>
2020-12-31 16:33:04 -0500

    se creo una funcion generateCalendarDays que permite refactorizar el código

--- js/index.js
+++ js/index.js
@@ -27,15 +27,27 @@ searchButton.onclick = function () {
   let dayNumbers = [...document.getElementsByClassName('day-number')];
   dayNumbers.forEach((day) => day.remove());
 
-  let newDayNumbers = '';
-
   let chosenDate = new Date(yearInput.value, monthInput.value);
   createCalendarTitle(chosenDate);
 
+  let daysGenerated = generateCalendarDays();
+
+  dayNames[6].insertAdjacentHTML('afterend', daysGenerated);
+};
+
+function createCalendarTitle(chosenDate) {
+  titleMontYear.textContent = `${chosenDate
+    .toDateString()
+    .substring(4, 7)} ${chosenDate.getFullYear()}`;
+}
+
+function generateCalendarDays() {
+  let days = '';
   for (let i = 1; i <= 31; i++) {
     let day = new Date(yearInput.value, monthInput.value, i);
+
     if (day.getMonth() == monthInput.value) {
-      newDayNumbers += `<li
+      days += `<li
                           class='day-number'
                           ${
                             i === 1
@@ -47,12 +59,5 @@ searchButton.onclick = function () {
                         >${day.getDate()}</li>`;
     }
   }
-
-  dayNames[6].insertAdjacentHTML('afterend', newDayNumbers);
-};
-
-function createCalendarTitle(chosenDate) {
-  titleMontYear.textContent = `${chosenDate
-    .toDateString()
-    .substring(4, 7)} ${chosenDate.getFullYear()}`;
+  return days;
 }
